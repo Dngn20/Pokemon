cb22801291b73dda9d986320d2eb4e98440ce732
Dngn20 <dngn20@gmail.com>
2016-12-01 12:50:33 -0700

    Pokemon Frame and Panel work

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,5 +1,36 @@
 package pokemon.view;
 
-public class PokemonPanel {
+import javax.swing.*;
+import pokemon.controller.PokemonController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private JButton upadateButton;
+	private JComboBox pokedexSelector;
+	private JLabel pokemonLabel;
+	private SpringLayout baseLayout;
+	private JLabel healthLabel;
+	private JLabel combatLabel;
+	private JLabel speedLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel advancedLabel;
+	private JTextField healthField;
+	private JTextField combatField;
+	private JTextField speedField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField advancedArea;
+	
+	public PokemonPanel(PokemonController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		
+		pokemonButton = new JButton("Got to catch them all!!!");
+	}
 }
